c966d4e7a455fa2330b475a2736568da38f7a29b
Kvu23 <kaushik.chauhan23@gmail.com>
2024-07-28 16:24:26 +0530

    added string practice programs

---
+++ Python/PracticeSet1/Practice7.py
@@ -0,0 +1,10 @@
+# WAP to enter user name and print its length
+
+name = input("enter user first name: ")
+print("first name is ",name)
+print("name length is ",len(name))
+
+#WAP to find $ character in string
+str = " i am multi millionoiare $ persion $$$"
+#print(str.find('$'))
+print("number of times $ occurs",str.count('$'))
