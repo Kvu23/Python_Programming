13e89265479a6386d6816bea16f862df0814a95a
Kvu23 <kaushik.chauhan23@gmail.com>
2025-11-22 17:18:12 +0530

    Added program to understand typecasting

---
+++ Python/Lecture12/Lecture12.py
@@ -0,0 +1,26 @@
+# this program is to understand typecasting in python
+
+# typecasting from string to integer
+num_str = "10"
+num_int = int(num_str)
+print("type of num_int is: ",type(num_int), num_int)
+
+# typecasting from float to integer
+num_float = 3.14
+num_int_from_float = int(num_float)
+print("type of num_int_from_float is: ",type(num_int_from_float), num_int_from_float)
+
+# typecasting from integer to float
+num_int2 = 20
+num_float_from_int = float(num_int2)
+print("type of num_float_from_int is: ",type(num_float_from_int), num_float_from_int)
+
+# typecasting from float to string
+num_float2 = 3.14159
+num_str_from_float = str(num_float2)
+print("type of num_str_from_float is: ",type(num_str_from_float), num_str_from_float)
+
+# typecasting from integer to string
+num_int3 = 100
+num_str_from_int = str(num_int3)
+print("type of num_str_from_int is: ",type(num_str_from_int), num_str_from_int)
