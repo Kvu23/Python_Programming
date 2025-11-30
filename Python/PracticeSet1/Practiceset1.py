2f518093bf03256a66410820df19e799654ad17f
Kvu23 <kaushik.chauhan23@gmail.com>
2025-11-30 17:55:52 +0530

    added practice set1 solutions

---
+++ Python/PracticeSet1/Practiceset1.py
@@ -0,0 +1,61 @@
+'''
+these are the solutions programs to practiceset1 from code with harry's python programming course
+'''
+#question1
+print("Hello, World! Welcome to Python.")
+
+#question2
+print("Twinkle, twinkle, little star,\nHow I wonder what you are!")
+
+#question3
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+height = float(input("Enter your height in meters: "))
+Student = True
+
+print("Name:", name, "Age:", age, "Height:", height, "m Student:", Student)
+
+#question4
+number = "45"
+number = int(number)
+number += 10
+print("The final number is:", number)
+
+# #question5
+food = input("Enter the name of your favourite food: ")
+
+print("Wow! I also like", food)
+
+#question6
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+print("Summation of", number1, "and", number2, "is:", number1 + number2)
+print("Subtraction of", number1, "and", number2, "is:", number1 - number2)
+print("Multiplication of", number1, "and", number2, "is:", number1 * number2)
+print("Quotionant of", number1, "and", number2, "is:", number1 / number2)
+
+#question7
+print("Hello \"python\" World!")
+print("This is on new line")
+print("This is a tab->\t after tab")
+
+#question8
+number = int(input("Enter a number: "))
+
+print("The square of", number, "is:", number ** 2)
+print("The cube of", number, "is:", number ** 3)
+
+print(int("10") + 5)
+
+
+
+
+
+
+
+
+
+
+
+
