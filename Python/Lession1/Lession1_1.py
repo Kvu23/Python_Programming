48db0d02a8e842edb34d0cbbd6b9a891de60072c
Kvu23 <kaushik.chauhan23@gmail.com>
2025-11-30 11:59:22 +0530

    Added new program to understand print statement and seperator and end arguments

---
+++ Python/Lession1/Lession1_1.py
@@ -0,0 +1,5 @@
+# this program realizes the print statement usages
+
+print('Hello world ! from Python in VS Code', "Kaushik Chauhan", 5, 6.9, sep=' , ')
+print('Hello world ! from Python in VS Code', end=' .. ')
+print('Hello world ! from Python in VS Code', end=' // ')
