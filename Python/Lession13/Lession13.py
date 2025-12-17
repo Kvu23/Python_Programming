bc45c5e69d3cff20f24f496d7d4c11447366f2d2
Kvu23 <kaushik.chauhan23@gmail.com>
2025-12-17 18:00:56 +0530

    Added sample program to understand If else statements

---
+++ Python/Lession13/Lession13.py
@@ -0,0 +1,11 @@
+# this is a sample Python file for undersatnding control flow in Python using if else statements
+
+age = int(input("Enter your age: "))
+
+if(age > 18):
+    print("You are eligible to vote.")
+elif(age == 18):
+    print("You have just become eligible to vote.")
+else:
+    print("You are not eligible to vote yet.")
+# End of Lession13.py
