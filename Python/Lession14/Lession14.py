4060237a9885935de01fe6f4b649c62863afacee
Kvu23 <kaushik.chauhan23@gmail.com>
2025-12-18 20:25:52 +0530

    Added example for while loop

--- Python/Lession14/Lession14.py
+++ Python/Lession14/Lession14.py
@@ -3,6 +3,15 @@
 
 number = int(input("Enter a number to print its multiplication table: "))
 
+print("\nUsing for loop to print multiplication table:")
 for i in range(1, 11):
     print(number, "x", i, "=", i * number)
     
+# same example using while loop
+print("\nUsing while loop to print multiplication table:")
+
+i = 1
+while i <= 10:
+    print(number, "x", i, "=", i * number)
+    i += 1
+
