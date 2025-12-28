6e474d36d1fc726102eacc36b4bdb0f28525141f
Kvu23 <kaushik.chauhan23@gmail.com>
2025-12-28 12:48:49 +0530

    Added sample exercise programs

---
+++ Python/Lession1/Lession1_3.py
@@ -0,0 +1,19 @@
+#this is sample input programs quiz
+
+people = int(input("How many people are there in the dinner group?: "))
+
+if people > 8:
+    print("You will have to wait for a table.") 
+else:
+    print("Your table is ready.")
+
+
+# multiple of 10
+number = int(input("Enter a number: "))
+
+if number % 10 == 0:
+    print("The number is a multiple of 10.")
+else:
+    print("The number is not a multiple of 10.")
+    
+
