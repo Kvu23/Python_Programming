9e52c48b305516ceeabbeefe85c48fde56f2e0c0
Kaushik Chauhan <kaushik.chauhan23@gmail.com>
2026-06-30 10:22:04 +0530

    Updated python programs and added new python programs for string

--- Python/Lession5/Lession5.py
+++ Python/Lession5/Lession5.py
@@ -2,13 +2,43 @@
 # these are different types of strings we can write it
 
 
-str1 = "This is a sample string"
-str2 = 'This is a sample string'
-str3 = """This is also a Sample string"""
-
+str1 = "This is a sample string by using double quotes"
 print(str1)
+str2 = 'This is a sample string by using single quotes'
 print(str2)
+str3 = """This is also a Sample string,
+        by using triple quotes it can expands upto multiple lines
+        as well as it can also be used for writing docstrings"""
 print(str3)
 
 str4 = "This is a sample string.\nIt has a new line character"
 print(str4)
+
+str = "Hello! World!"
+print("length of the string is:- ",len(str)) # this will print the length of the string
+
+print(str[0]) # this will print the first character of the string
+print(str[1]) # this will print the second character of the string
+print(str[2]) # this will print the third character of the string  
+print(str[3]) # this will print the fourth character of the string
+print(str[4]) # this will print the fifth character of the string
+print(str[5]) # this will print the sixth character of the string
+print(str[6]) # this will print the seventh character of the string
+print(str[7]) # this will print the eighth character of the string
+print(str[8]) # this will print the ninth character of the string
+print(str[9]) # this will print the tenth character of the string
+print(str[10]) # this will print the eleventh character of the string
+print(str[11]) # this will print the twelfth character of the string
+print(str[12]) # this will print the thirteenth character of the string
+
+print(str[::-1]) # this will print the string in reverse order
+
+print(str[0:5]) # this will print the first five characters of the string
+print(str[6:12]) # this will print the string from the sixth to twelfth character
+print(str[0:12:2]) # this will print the string from the first to twelfth character with a step of 2
+
+print(str[::2]) # this will print the string with a step of 2
+print(str[-1:-14:-1]) # this will print the string in reverse order from the last character to the first character
+
+
+
