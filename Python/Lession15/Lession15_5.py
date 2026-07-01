c8b8129b719761c2d6552348da728e7a6a4150d5
Kaushik Chauhan <kaushik.chauhan23@gmail.com>
2026-07-01 14:38:28 +0530

    updated python program for functions scopes and doc strings

--- Python/Lession15/Lession15_5.py
+++ Python/Lession15/Lession15_5.py
@@ -3,17 +3,23 @@
 z = 10 # global variable
 
 def sum(a, b):
+    '''this program will
+    add two numbers and print the sum'''
     z = a + b # local variable
     print("Sum is:", z)
     
+print(sum.__doc__)
 sum(2,7)
 print("Global variable z is:",z)
 
 def myfunc():
+    '''this function will change the value of global variable z'''
+    
     global z # to use the global variable z
     z = 5 # changing the value of global variable z
     print("Value of global variable z inside myfunc is:",z)
-    
+
+print(myfunc.__doc__)
 myfunc()
 print("Value of global variable z outside myfunc after modifications is:",z)
 
